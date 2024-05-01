0e8873a2dbe902d5fb75cc4891504a40072f6afe
BoMin <suhpmp@gmail.com>
2024-05-01 17:32:39 +0900

    CHORE: Initial commit

---
+++ finpal-backend/src/main/kotlin/com/cafemocha/finpalbackend/FinpalBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.cafemocha.finpalbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinpalBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinpalBackendApplication>(*args)
+}
---
+++ finpal-backend/src/test/kotlin/com/cafemocha/finpalbackend/FinpalBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cafemocha.finpalbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinpalBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
