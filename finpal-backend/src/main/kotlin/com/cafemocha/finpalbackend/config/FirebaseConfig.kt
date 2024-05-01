5a2bb7b4671c017ab0b75bfe4fc45114b90d4180
BoMin <suhpmp@gmail.com>
2024-05-01 19:06:46 +0900

    FEAT: Updata FirebaseConfig.kt

--- finpal-backend/src/main/kotlin/com/cafemocha/finpalbackend/config/FirebaseConfig.kt
+++ finpal-backend/src/main/kotlin/com/cafemocha/finpalbackend/config/FirebaseConfig.kt
@@ -3,6 +3,7 @@ package com.cafemocha.finpalbackend.config
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.auth.FirebaseAuth
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,16 +11,23 @@ import org.springframework.core.io.Resource
 
 @Configuration
 class FirebaseConfig {
-    @Value("\${firebase.config.path")
+    @Value("\${firebase.config.path}")
     private lateinit var firebaseConfigPath: Resource
 
     @Bean
-    fun firebaseInitialization() {
+    fun firebaseInitialization(): FirebaseApp {
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(firebaseConfigPath.inputStream))
             .build()
+
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options)
         }
+        return FirebaseApp.getInstance()
+    }
+
+    @Bean
+    fun firebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance(firebaseInitialization())
     }
 }
